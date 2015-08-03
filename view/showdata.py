389201253f862e76da782dbcaf0645bc4c8f3735
LouYu <1521664795@qq.com>
2015-08-03 16:28:20 +0800

    Initial Commit

---
+++ view/showdata.py
@@ -0,0 +1,17 @@
+﻿from django.shortcuts import render
+import csv
+
+def showTablePage(request):
+    '''
+    Display a table for a request
+    '''
+    return showTable(request, 'a', 'b')
+    
+def showTable(request, path, csv):
+    '''
+    Retrurn a httpRespond.
+    
+    path: The path that will be displayed on the view
+    csv: An standerd csv object that will be displayed
+    '''
+    return render(request, 'showdata/table.html', {'title': '测试标题'})
