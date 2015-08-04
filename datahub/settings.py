25d2c58ad318fbe43b15cf6986df0e8fbd691b62
LouYu <1521664795@qq.com>
2015-08-04 16:44:18 +0800

    Writed template for showing a table

--- datahub/settings.py
+++ datahub/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'pagination', 
 )
 
 MIDDLEWARE_CLASSES = (
@@ -48,6 +49,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'pagination.middleware.PaginationMiddleware',
 )
 
 ROOT_URLCONF = 'datahub.urls'
@@ -63,6 +65,11 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+				"django.contrib.auth.context_processors.auth",  
+				"django.core.context_processors.debug",  
+				"django.core.context_processors.i18n",  
+				"django.core.context_processors.media",  
+				"django.core.context_processors.request" ,
             ],
         },
     },
