d5ab2d87949e41ea057c19293feefd9a0ea853a7
LouYu <1521664795@qq.com>
2015-08-04 18:30:50 +0800

    Almost finished writting interface for displaying a table.

--- view/showdata.py
+++ view/showdata.py
@@ -1,17 +1,43 @@
 ﻿from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 import csv
 
 def showTablePage(request):
     '''
     Display a table for a request
     '''
-    return showTable(request, 'a', 'b')
+    csvfile = file(r'test\ex2data2.txt', 'rb')
+    reader = csv.reader(csvfile)
+    
+    respond = showTable(request, r'test\ex2data2.txt', reader)
+    
+    csvfile.close() 
+    return respond
     
 def showTable(request, path, csv):
     '''
     Retrurn a httpRespond.
     
     path: The path that will be displayed on the view
-    csv: An standerd csv object that will be displayed
+    csv: An standerd csv reader that will be displayed
     '''
-    return render(request, 'showdata/table.html', {'title': '测试标题'})
+    
+    lines = list(csv)
+    paginator = Paginator(lines[1:], 20)
+    
+    page = request.GET.get('page', 1)
+    
+    try:
+        content = paginator.page(page)
+    except PageNotAnInteger:
+        content = paginator.page(1)
+    except EmptyPage:
+        content = paginator.page(paginator.num_pages)
+    
+    return render(request, 'showdata/table.html', 
+    {'title': '测试标题',
+    'path': path, 
+    'page_range': paginator.page_range,
+    'paginator': paginator,
+    'content': content,
+    'table_headings': list(lines[0])})
